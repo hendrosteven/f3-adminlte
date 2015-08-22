f006acadfb2b6b4d449699034be3fe2d780ea164
hendro steven <hendro.steven@gmail.com>
2015-08-22 14:07:48 +0700

    update

--- appdb.sql
+++ appdb.sql
@@ -6,13 +6,11 @@
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8*/;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 -- Dumping database structure for appdb
-CREATE DATABASE IF NOT EXISTS `appdb` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `appdb`;
 
 
 -- Dumping structure for table appdb.accounts
